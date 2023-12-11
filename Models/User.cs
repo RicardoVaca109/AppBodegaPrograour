e9e3e42c6e7da42bf0f23911782af96faff73d9b
Ricardo.Vaca <ricardo.vaca@udla.edu.ec>
2023-12-10 23:50:20 -0500

    ValidacionCompletaFaltaBloqueo

--- Models/User.cs
+++ Models/User.cs
@@ -4,14 +4,16 @@ namespace appBodega.Models
 {
     public class User
     {
-        [Required(ErrorMessage ="ID es requerido para continuar")]
+        [Required]
         public int IdUser { get; set; }
-        [Required(ErrorMessage = "Mail del ususario es requerido para continuar")]
+        [Required]
         public string UserMail { get; set; }
-        [Required(ErrorMessage = "Contraseña es requerida para continuar")]
-        [DataType(DataType.Password)]
+        [Required]
+        //[DataType(DataType.Password)]
         public string UserPassword { get; set; }
+        //[Required]
+        public string ConfirmPassword { get; set; }
 
-       
+
     }
 }
